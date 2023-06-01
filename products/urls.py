3a8e05adc141430b6f4fca1cb7c2e266cd74b322
engulfedInFlames <recona97@gmail.com>
2023-06-01 19:03:10 +0900

    Tiny Changes

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path("products/<int:_id>/", views.detail_view),
-    path("products/<int:_id>/inventory", views.inventory_view),
+    path("products/<int:_id>/inventory/", views.inventory_view),
     path("products/register/", views.register),
     path("products/<int:_id>/delete/", views.delete),
     path("products/order/", views.order),
-    path("products/<int:_id>/inbound", views.inbound),
-    path("products/<int:_id>/outbound", views.outbound),
+    path("products/<int:_id>/inbound/", views.inbound),
+    path("products/<int:_id>/outbound/", views.outbound),
 ]
